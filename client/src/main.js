ef43518ec0a6464943015d254c89b739aedc388b
Kevin Chan <kevindchan@gmail.com>
2020-04-26 14:09:47 -0700

    CLIENT - Adds vue router and simple vue validator

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
+import router from "./router";
+import SimpleVueValidation from 'simple-vue-validator';
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount("#app");
+
+Vue.use(SimpleVueValidation);
